2cae61c56206b3b9213fa6a8f76fc9c97f192d30
“piyushmathur17” <piyushmathur17@gmail.com>
2020-09-30 00:51:44 +0530

    main module

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include<bits/stdc++.h>
 #include "experiment.cpp"
 #include "test.cpp"
-using namespace std;
 
 int main(){
 
 	int N = 3;//Number of experiments
-	
+
 	//test random function
 	Test T;
 	
-	std::cout<<"Conducting Tests: \n\n";
+	std::cout<<"Conducting Tests: \n--------------------\n";
 	
 	if(T.is_uniform())
-	    std::cout<<"The random function is uniform\n\n";
+	    std::cout<<"\nThe random function is uniform\n\n";
 	else 
-		std::cout<<"The random function seems skewed, the results may be inaccurate\n\n";
+		std::cout<<"\nThe random function seems skewed, the results may be inaccurate\n\n";
 
+	std::cout<<"------------------------------------------------------\n";
 	//perform N experiments
-	std::cout<<"\nConducting Experiments: \n\n";
+	std::cout<<"\nConducting Experiments: \n------------------------\n";
 
 	for(int i=0;i<N;i++){
 		Experiment E;
